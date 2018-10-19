9dfae31cedbcc17892d469090c59b6bd9be6748f
Little@Yang <1938871284@qq.com>
2018-10-19 19:27:12 +0800

    第一次构建

---
+++ 1.helloworld/QtGuiApplication1/QtGuiApplication1/GeneratedFiles/ui_QtGuiApplication1.h
@@ -0,0 +1,81 @@
+/********************************************************************************
+** Form generated from reading UI file 'QtGuiApplication1.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.6
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_QTGUIAPPLICATION1_H
+#define UI_QTGUIAPPLICATION1_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_QtGuiApplication1Class
+{
+public:
+    QWidget *centralWidget;
+    QLabel *label;
+    QLabel *label_2;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *QtGuiApplication1Class)
+    {
+        if (QtGuiApplication1Class->objectName().isEmpty())
+            QtGuiApplication1Class->setObjectName(QStringLiteral("QtGuiApplication1Class"));
+        QtGuiApplication1Class->resize(600, 400);
+        centralWidget = new QWidget(QtGuiApplication1Class);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(210, 80, 161, 16));
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(500, 300, 54, 12));
+        QtGuiApplication1Class->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(QtGuiApplication1Class);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 600, 23));
+        QtGuiApplication1Class->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(QtGuiApplication1Class);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        QtGuiApplication1Class->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(QtGuiApplication1Class);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        QtGuiApplication1Class->setStatusBar(statusBar);
+
+        retranslateUi(QtGuiApplication1Class);
+
+        QMetaObject::connectSlotsByName(QtGuiApplication1Class);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *QtGuiApplication1Class)
+    {
+        QtGuiApplication1Class->setWindowTitle(QApplication::translate("QtGuiApplication1Class", "QtGuiApplication1", Q_NULLPTR));
+        label->setText(QApplication::translate("QtGuiApplication1Class", "\350\277\231\346\230\257\346\210\221\347\232\204\347\254\254\344\270\200\344\270\252QT\345\272\224\347\224\250\347\250\213\345\272\217\357\274\201", Q_NULLPTR));
+        label_2->setText(QApplication::translate("QtGuiApplication1Class", "\346\235\250\346\231\250", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class QtGuiApplication1Class: public Ui_QtGuiApplication1Class {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_QTGUIAPPLICATION1_H
