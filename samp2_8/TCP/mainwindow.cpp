8b0be3254b05690eac10f5f36e42d073904890f6
e <1938871284@qq.com>
2019-01-26 10:20:04 +0800

    no message

--- samp2_8/TCP/mainwindow.cpp
+++ samp2_8/TCP/mainwindow.cpp
@@ -1,14 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QTcpServer>
+#include <QTcpSocket>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    mServer = new QTcpServer();
+
+    mServer->listen(QHostAddress::Any,9988);
+
+    connect(mServer,SIGNAL(newConnection()),this,SLOT(new_client()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+
+
